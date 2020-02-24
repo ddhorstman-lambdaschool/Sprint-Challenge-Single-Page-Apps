5ecd6ffd0efdbe2f81dd712575917923c051e1e3
ddhorstman <davidzilla27@gmail.com>
2020-02-23 23:18:30 -0500

    Stretch: Don't call API until done typing search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+let submissionTimeout = 0;
 
 export default function SearchForm({ searchTerm, setSearchTerm }) {
+
+  const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm);
+  
+  function submitSearch() {
+    setSearchTerm(localSearchTerm);
+    //console.log("Search submitted!", localSearchTerm);
+  }
 
   return (
     <section className="search-form">
       <input
         placeholder="Search by name"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)} />
+        value={localSearchTerm}
+        onChange={e => {
+          setLocalSearchTerm(e.target.value);
+          //console.log("Search reset", localSearchTerm);
+          window.clearTimeout(submissionTimeout);
+          submissionTimeout = window.setTimeout(submitSearch, 500);
+        }} />
     </section>
   );
 }
